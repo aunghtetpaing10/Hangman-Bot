9028fb23baeb80cada37f73703e597defb2e653e
Aung Htet Paing <aunghtetpaing211199@gmail.com>
2024-07-02 11:36:15 +0700

    first commit

---
+++ telegram-bot.py
@@ -0,0 +1,79 @@
+import random
+from words import words
+from hangman_visual import lives_visual_dict
+import string
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, ContextTypes, filters, Application
+
+TOKEN = "7229266823:AAEQxHTL7s_Xh1MTvL0ZPPvuvesckm27yL8"
+
+games = {}
+
+def get_random_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.message.chat_id
+    word = get_random_word(words)
+    games[chat_id] = {
+        "word": word,
+        "word_letters": set(word),
+        "guessed_letters": set(),
+        "lives": 7
+    }
+    await update.message.reply_text(f"Let's play Hangman! You have 7 lives. Guess a letter by typing it in.")
+
+
+async def guess(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    alphabet = string.ascii_uppercase
+
+    chat_id = update.message.chat_id
+    if chat_id not in games:
+        await update.message.reply_text("Start a new game by typing /start")
+        return
+    
+    game = games[chat_id]
+    user_guess = update.message.text.upper()
+
+    if user_guess in alphabet and len(user_guess) == 1:
+        if user_guess in game['guessed_letters']:
+            await update.message.reply_text("You have already guessed that letter. Try again.")
+        else:
+            game['guessed_letters'].add(user_guess)
+            if user_guess in game['word_letters']:
+                game['word_letters'].remove(user_guess)
+                if not game['word_letters']:
+                    await update.message.reply_text(f"You won! The word was {game['word']}. Start a new game with /start")
+                    del games[chat_id]
+                    return
+            else:
+                game['lives'] -= 1
+                if game['lives'] == 0:
+                    await update.message.reply_text(f"Game Over. The correct word was {game['word']}. Start a new game with /start")
+                    del games[chat_id]
+                    return
+    
+            display_word = ' '.join([letter if letter in game['guessed_letters'] else '_' for letter in game['word']])
+            message = (f"{lives_visual_dict[game['lives']]}\n"
+                       f"Current word: {display_word}\n"
+                       f"You have {game['lives']} lives left and have guessed these letters: {' '.join(game['guessed_letters'])}")
+            await update.message.reply_text(message)
+    else:
+        await update.message.reply_text("That is not a valid letter. Please guess a single letter.")
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, guess ))
+
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
